ade1d75900cd59ae7a1a28b978beca65916e4e22
Vihan Agarwal <vihan.agarwal@CTMCWGR972.local>
2025-01-29 15:25:55 +0530

    sqs listener working

--- src/main/java/com/cleartax/training_superheroes/services/SuperheroConsumer.java
+++ src/main/java/com/cleartax/training_superheroes/services/SuperheroConsumer.java
@@ -4,7 +4,7 @@ import com.cleartax.training_superheroes.config.SqsConfig;
 import com.cleartax.training_superheroes.entities.Superhero;
 import com.cleartax.training_superheroes.repos.SuperheroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.*;
@@ -20,42 +20,64 @@ public class SuperheroConsumer {
   @Autowired
   private SuperheroRepository superheroRepository;
 
-  @Scheduled(fixedDelay = 1000)
-  public void consumeSuperhero() {
-    try {
-      ReceiveMessageResponse receiveMessageResponse = sqsClient.receiveMessage(ReceiveMessageRequest.builder()
-              .queueUrl(sqsConfig.getQueueUrl())
-              .maxNumberOfMessages(1)  // Adjust according to your needs
-              .waitTimeSeconds(10) // Enable long polling
-              .build());
+  // Commenting out the scheduled method
+  // @Scheduled(fixedDelay = 1000)
+  // public void consumeSuperhero() {
+  //   try {
+  //     ReceiveMessageResponse receiveMessageResponse = sqsClient.receiveMessage(ReceiveMessageRequest.builder()
+  //             .queueUrl(sqsConfig.getQueueUrl())
+  //             .maxNumberOfMessages(1)  // Adjust according to your needs
+  //             .waitTimeSeconds(10) // Enable long polling
+  //             .build());
+  //
+  //     receiveMessageResponse.messages().forEach(message -> {
+  //       String body = message.body();
+  //       System.out.println("Received message: " + body);
+  //       if (body.contains("vihanagarwal")) {
+  //         body = body.replace("vihanagarwal", "vihanagarwal updated");
+  //         System.out.println("Updated message: " + body);
+  //         List<Superhero> superheroes = superheroRepository.findAllByName("vihanagarwal");
+  //         if (!superheroes.isEmpty()) {
+  //           for (Superhero superhero : superheroes) {
+  //             superhero.setName("vihanagarwal updated");
+  //             superheroRepository.save(superhero);
+  //           }
+  //           System.out.println("Superhero name updated in the database.");
+  //         } else {
+  //           System.out.println("Superhero with name 'vihan' not found in the database.");
+  //         }
+  //       } else {
+  //         System.out.println("Original message: " + body);
+  //       }
+  //       sqsClient.deleteMessage(DeleteMessageRequest.builder()
+  //               .queueUrl(sqsConfig.getQueueUrl())
+  //               .receiptHandle(message.receiptHandle())
+  //               .build());
+  //     });
+  //   } catch (SqsException e) {
+  //     System.err.println("SQS error: " + e.awsErrorDetails().errorMessage());
+  //     System.out.println("The queue might be empty!!");
+  //   }
+  // }
 
-      receiveMessageResponse.messages().forEach(message -> {
-        String body = message.body();
-        System.out.println("Received message: " + body);
-        if (body.contains("vihanagarwal")) {
-          body = body.replace("vihanagarwal", "vihanagarwal updated");
-          System.out.println("Updated message: " + body);
-          List<Superhero> superheroes = superheroRepository.findAllByName("vihanagarwal");
-          if (!superheroes.isEmpty()) {
-            for (Superhero superhero : superheroes) {
-              superhero.setName("vihanagarwal updated");
-              superheroRepository.save(superhero);
-            }
-            System.out.println("Superhero name updated in the database.");
-          } else {
-            System.out.println("Superhero with name 'vihan' not found in the database.");
-          }
-        } else {
-          System.out.println("Original message: " + body);
+  @SqsListener("${sqs.queue.name}")
+  public void listenToQueue(String messageBody) {
+    System.out.println("Received message: " + messageBody);
+    if (messageBody.contains("vihanagarwal")) {
+      messageBody = messageBody.replace("vihanagarwal", "vihanagarwal updated");
+      System.out.println("Updated message: " + messageBody);
+      List<Superhero> superheroes = superheroRepository.findAllByName("vihanagarwal");
+      if (!superheroes.isEmpty()) {
+        for (Superhero superhero : superheroes) {
+          superhero.setName("vihanagarwal updated");
+          superheroRepository.save(superhero);
         }
-        sqsClient.deleteMessage(DeleteMessageRequest.builder()
-                .queueUrl(sqsConfig.getQueueUrl())
-                .receiptHandle(message.receiptHandle())
-                .build());
-      });
-    } catch (SqsException e) {
-      System.err.println("SQS error: " + e.awsErrorDetails().errorMessage());
-      System.out.println("The queue might be empty!!");
+        System.out.println("Superhero name updated in the database.");
+      } else {
+        System.out.println("Superhero with name 'vihan' not found in the database.");
+      }
+    } else {
+      System.out.println("Original message: " + messageBody);
     }
   }
 }
